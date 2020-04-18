3b07971af0585750cfbd132f059c7e98b454e455
saquib <Tisa3ma123>
2020-04-18 07:35:31 +0500

    code changed in feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello dsdsd
+          Edit <code>src/App.js</code> and save to reload. I was changed on the feature branch
         </p>
         <a
           className="App-link"
